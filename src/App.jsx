5a761c1f218b26bcb869c923ba4cdd77d00cad75
Jomar Rumbawa <121916351+jrumbawa@users.noreply.github.com>
2023-06-02 14:26:15 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import './App.css'
 
 function App() {
   const [todos, setTodos] = useState(['todo 1', 'todo 2'])
+  const [todo, setTodo] = useState('')
 
   const todosElement = todos.map((todo) => <li>{todo}</li>)
 
   function addTodo() {
-    setTodos((prevTodos) => [...prevTodos, `thing ${todos.length + 1}`])
+    setTodos((prevTodos) => [...prevTodos, todo])
+    setTodo('')
+  }
+
+  function handleTodo(event) {
+    setTodo(event.target.value)
   }
 
   return (
     <>
-      <input type="text" />
+      <input type="text" value={todo} onChange={handleTodo} />
       <button onClick={addTodo}>Submit</button>
       <ul>{todosElement}</ul>
     </>
